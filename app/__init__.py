3224e13bf61f7236cd1c128161c68f156274b4db
jonathanharijanto <harijant@uci.edu>
2020-01-05 21:28:51 -0800

    over the break update

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 # __init__.py
 
+# third-party imports
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+
+# local imports
 from config import app_config
 
 db = SQLAlchemy()
-
 # user management, logging in, logging out, and user sessions.
 login_manager = LoginManager()
 
@@ -15,8 +18,9 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     # message when a user is redirected to the login page.
     login_manager.login_message = "You must be logged in to access this page."
@@ -34,5 +38,14 @@ def create_app(config_name):
     migrate = Migrate(app, db)
 
     from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
